a33e6ac1609233173f84638d438521e3417be92b
Sudhit Chauhan <sudhit@webmynesystems.com>
2023-03-20 15:38:30 +0530

    sadsadsa

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 each "Build Start"
 python3.9 -m pip install -r requirements.txt
-#python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
+each "Build End"
 each "Build End"
